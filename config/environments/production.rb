c2f9c22c58975e6a9e80e3bf5874169497b28f0e
EC2 Default User <ec2-user@ip-172-31-17-99.us-east-2.compute.internal>
2020-02-02 11:11:13 +0000

    37commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.assets.compile = true
+  config.serve_static_assets = true
   #config.public_file_server.enabled = true
 
 end
