87189a53b371cf53ae353841e944f93f72970572
EC2 Default User <ec2-user@ip-172-31-17-99.us-east-2.compute.internal>
2020-02-02 10:13:16 +0000

    32commit

--- db/migrate/20191018043909_change_column_to_staff_2.rb
+++ db/migrate/20191018043909_change_column_to_staff_2.rb
@@ -1,6 +1,6 @@
 class ChangeColumnToStaff2 < ActiveRecord::Migration[5.0]
   def up
-    change_column :staffs, :status, 'integer USING CAST(hoge AS integer)'
+    change_column :staffs, :status, 'integer USING CAST(status AS integer)'
   end
   
   def down
