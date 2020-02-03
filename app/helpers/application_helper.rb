09a62ba3f16c8128cb29fe43db6266ffa44bf7e8
EC2 Default User <ec2-user@ip-172-31-17-99.us-east-2.compute.internal>
2020-02-03 14:23:19 +0000

    46commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,8 @@ module ApplicationHelper
     
     #共通のhelperメソッド
     def number_to_currency(menu)
-        "￥#{menu.price.to_s(:delimited)}"
+        "￥#{menu.price.to_s(:delimited, delimiter: ',')}"
     end
     
-    
-    
-    
+
 end
